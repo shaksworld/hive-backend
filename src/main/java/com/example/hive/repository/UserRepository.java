5e4df77633efda97dd70ef70732dfcba4710dc33
iamstevol <stevol2015@gmail.com>
2023-03-14 22:34:55 +0100

    Changed the query to findByEmail

--- src/main/java/com/example/hive/repository/UserRepository.java
+++ src/main/java/com/example/hive/repository/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
-    Optional<User> getUserByEmail(String email);
+    Optional<User> findByEmail (String email);
 }
